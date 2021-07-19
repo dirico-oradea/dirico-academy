b806d290f662e72f114bbfa194f94f3cf54bfc62
Zoltan Cs <cserges@gmail.com>
2021-07-19 22:12:25 +0300

    ...

--- js/test.js
+++ js/test.js
@@ -1,1 +1,8 @@
 console.log('1+6=', 1+6);
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello Dolly!";
+    
+  }
+  
